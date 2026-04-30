6c1a742c51e03fdaee947003ea5c2cc4b6eb02fa
Zoe Mitchell <mitchellzh101@gmail.com>
2026-04-30 19:33:06 -0400

    Add: string-encode-and-decode - submission-7

---
+++ Data Structures & Algorithms/string-encode-and-decode/submission-7.py
@@ -0,0 +1,21 @@
+class Solution:
+
+    def encode(self, strs: List[str]) -> str:
+        ret = ""
+        delimiter = "ZoezoeZoe"
+        for word in strs:
+            ret+=delimiter+str(len(word))+delimiter+word
+        return ret
+
+    def decode(self, s: str) -> List[str]:
+        delimiter = "ZoezoeZoe"
+        chunks = s.split(delimiter)
+        ret = []
+        ndx = 0
+        while ndx < len(chunks):
+            if chunks[ndx].isdigit():
+                ret.append(chunks[ndx+1])
+                ndx += 2
+            else:
+                ndx += 1
+        return ret
