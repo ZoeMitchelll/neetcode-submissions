f5b6d6e13c4b2691780aade7b72c2b4bb5c1e00a
Zoe Mitchell <mitchellzh101@gmail.com>
2026-04-30 15:36:42 -0400

    Add: anagram-groups - submission-2

---
+++ Data Structures & Algorithms/anagram-groups/submission-2.py
@@ -0,0 +1,13 @@
+from collections import defaultdict
+class Solution:
+    def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
+        #dictionary of a set mapped to a list and then for every string make a set of it and compare (time n*m space n)
+        #sort every string and get the ndx of same strings and return the ndx of origional list grouped together (time nlogn+n space n)
+        groups = defaultdict(list)
+        for string in strs:
+            freq = [0]*26
+            for character in string:
+                freq[ord(character) - ord('a')] = freq[ord(character) - ord('a')] + 1
+            groups[tuple(freq)].append(string)
+        return list(groups.values())
+        
